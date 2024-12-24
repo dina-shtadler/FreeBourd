d912a2071803e418261f7a9478a29f00dc98f812
dina shtadler <dina-shtadler@users.noreply.github.com>
2024-12-24 13:33:29 +0200

    Update site for GitHub Pages

--- src/SummerApartment/Main.jsx
+++ src/SummerApartment/Main.jsx
@@ -2,7 +2,7 @@ import { Apartments } from "./apartments"
 import { Home } from "./home"
 import { HashRouter } from 'react-router-dom';
 
-import { BrowserRouter } from "react-router-dom";
+// import { BrowserRouter } from "react-router-dom";
 import { Nav } from "./Nav";
  import { Routing } from "./Routing";
 export const Main = () => {
