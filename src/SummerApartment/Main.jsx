609e5d29d71bc14ad86d7e02692143875fbc5c14
dina shtadler <dina-shtadler@users.noreply.github.com>
2024-12-24 13:15:17 +0200

    Update site for GitHub Pages

--- src/SummerApartment/Main.jsx
+++ src/SummerApartment/Main.jsx
@@ -1,19 +1,21 @@
 import { Apartments } from "./apartments"
 import { Home } from "./home"
+import { HashRouter } from 'react-router-dom';
+
 import { BrowserRouter } from "react-router-dom";
 import { Nav } from "./Nav";
  import { Routing } from "./Routing";
 export const Main = () => {
     return <>
 
-    <BrowserRouter >
+    <HashRouter >
 {/* nav טוענת את ה */}
 <Nav >
 </Nav>
 {/* <Home></Home> */}
 {/* טוענת את כל הצהרות הניתובים */}
 <Routing  ></Routing>
-</BrowserRouter>
+</HashRouter>
         {/* <Apartments></Apartments> */}
     </>
 }
